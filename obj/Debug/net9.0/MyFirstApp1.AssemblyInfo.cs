a6af3109f244f90e21805c611d62abd7dd5588ce
Kalyani <kalyani.nelluri54@gmail.com>
2026-05-18 12:26:41 +0100

    advanced todo-app

--- obj/Debug/net9.0/MyFirstApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyFirstApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fbc1880869d04e10cb226968648c6dcd96b5b70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85678cfe0be229117407bd9a14ed5b9205dfebc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
